3ea1284e863be117e44a79b3e5ff0470ec9dd2b4
Arthur Koba <caprimores@gmail.com>
2024-07-24 03:19:19 +0300

    added Platform Limitation - ESP32 Only

--- src/AsyncInput.h
+++ src/AsyncInput.h
@@ -1,6 +1,10 @@
 #ifndef ASYNCINPUT_ASYNCINPUT_H
 #define ASYNCINPUT_ASYNCINPUT_H
 
+#if not defined(CONFIG_IDF_TARGET_ESP32) or not CONFIG_IDF_TARGET_ESP32
+#error THE LIBRARY IS CURRENTLY ONLY AVAILABLE ON ESP32
+#endif
+
 #if not defined(ASYNCINPUT_BUTTON_H)
 #include "button.h"
 #endif
